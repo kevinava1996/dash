ca0ba1e496d58017b173a7ad369051998c9b2ef7
kevinava1996 <39483772+kevinava1996@users.noreply.github.com>
2018-05-21 12:56:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ var dashboard = new ParseDashboard({
       appId: process.env.APP_ID || 'A1P2P3I4D5',
       masterKey: process.env.MASTER_KEY || 'M1A2S3T4E5R6K7E8Y9',
       serverURL: process.env.SERVER_URL || 'https://pushnotif.scalingo.io',
-      appName: process.env.APP_NAME || 'MyApp',
+      appName: process.env.APP_NAME || 'APPPARSE',
     },
   ],
   users: [
